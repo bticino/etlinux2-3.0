9661f21c1cd5e9c00ca14d5af7c7756839551c0e
cvs <cvs>
2006-06-14 07:54:33 +0000

    Initial revision

---
+++ etlinux-3.0/Configs/Etlinux.mk
@@ -0,0 +1,8 @@
+
+# This is included by the top Makefile. You can specify your
+# customizations for this specific configuration, or whatever.
+
+# As of version 3.0, using Etlinux.cfg in the build dir is suggested.
+# Such file should define CONFIG, PLATFORM, CUSTOM
+
+# CFG_CUSTOM = this that andthattoo
